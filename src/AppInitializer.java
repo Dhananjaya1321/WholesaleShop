d46401a3be1ae005c846d95f431bc623d0eecef8
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-02-25 23:19:58 -0800

    Create HomeForm and controller class

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,6 +1,9 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AppInitializer extends Application {
 
@@ -9,7 +12,14 @@ public class AppInitializer extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(
+                new Scene(
+                        FXMLLoader.load(
+                                getClass().getResource("lk/ijse/wholesale_shop/view/HomeForm.fxml")
+                        )
+                )
+        );
+        primaryStage.show();
     }
 }
---
+++ src/lk/ijse/wholesale_shop/controller/HomeFormController.java
@@ -0,0 +1,38 @@
+
+package lk.ijse.wholesale_shop.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class HomeFormController {
+
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private JFXButton btnCustomer;
+
+    @FXML
+    private JFXButton btnItem;
+
+    @FXML
+    private JFXButton btnOrder;
+
+    @FXML
+    void btnCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnItemOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderOnAction(ActionEvent event) {
+
+    }
+
+}
