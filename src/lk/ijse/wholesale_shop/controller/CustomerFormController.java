53da168fa2e552981b9d3909a2610f2d9392e294
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-02-26 01:21:00 -0800

    Create CustomerForm and CustomerFormController

---
+++ src/lk/ijse/wholesale_shop/controller/CustomerFormController.java
@@ -0,0 +1,68 @@
+package lk.ijse.wholesale_shop.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
+public class CustomerFormController {
+
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private JFXButton btnAdd;
+
+    @FXML
+    private TableView<?> table;
+
+    @FXML
+    private TableColumn<?, ?> cName;
+
+    @FXML
+    private TableColumn<?, ?> cDOB;
+
+    @FXML
+    private TableColumn<?, ?> cAddress;
+
+    @FXML
+    private TableColumn<?, ?> cCountact;
+
+    @FXML
+    private JFXButton btnUpdate;
+
+    @FXML
+    private JFXButton btnDelete;
+
+    @FXML
+    private JFXTextField txtCountact;
+
+    @FXML
+    private JFXTextField txtName;
+
+    @FXML
+    private JFXTextField txtDOB;
+
+    @FXML
+    private JFXTextArea txtAddress;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+}
